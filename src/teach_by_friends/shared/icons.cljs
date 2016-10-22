499f16ad39ab8817a76d7803e4b1de047d85e8f8
Alexey Savelev <savelichalex93@gmail.com>
2016-10-22 20:52:48 +0300

    Fix error with dinamic paths to images (remake to static)

--- src/teach_by_friends/shared/icons.cljs
+++ src/teach_by_friends/shared/icons.cljs
@@ -1,15 +1,19 @@
 (ns teach-by-friends.shared.icons)
 
-(defn import-icon [name]
-  (js/require (str "./images/" name ".png")))
+(def back (js/require "./images/back.png"))
+(def close (js/require "./images/close.png"))
+(def episodes (js/require "./images/episodes.png"))
+(def favorites (js/require "./images/favorites.png"))
+(def information (js/require "./images/information.png"))
+(def words (js/require "./images/words_active.png"))
 
 (def ^:private icons
-  {:back (import-icon "back")
-   :close (import-icon "close")
-   :episodes (import-icon "episodes")
-   :favorites (import-icon "favorites")
-   :information (import-icon "information")
-   :words (import-icon "words_active")})
+  {:back back
+   :close close
+   :episodes episodes
+   :favorites favorites
+   :information information
+   :words words})
 
 (defn get-icon [icon]
   (get icons icon))
