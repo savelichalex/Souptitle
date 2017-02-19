61a7156d2a9ddf3dba4b8c2e3644e16840f0eb0f
Alexey Savelev <savelichalex93@gmail.com>
2017-02-19 13:28:41 +0300

    mobile: Use actual well known screen

--- apps/mobile/src/souptitle_mobile/shared/scenes/well_known_screen.cljs
+++ apps/mobile/src/souptitle_mobile/shared/scenes/well_known_screen.cljs
@@ -22,7 +22,8 @@
         [ui/view {:style {:flex 1
                           :flex-direction "column"
                           :align-items (if words-empty? "center" "stretch")
-                          :justify-content "center"}}
+                          :justify-content "center"
+                          :margin-top 20}}
          (if words-empty?
            [empty-well-known-words]
            [table-view {:num-rows   (count @well-known-words)
@@ -37,25 +38,6 @@
                         :back-color "black"
                         :style      {:flex 1}}])]))))
 
-
-;; (defscreen
-;;   well-known-screen
-;;   ([props]
-;;    [well-known-words-comp props])
-;;   ([_]
-;;    {:title          "Favorite"
-;;     :navigatorStyle {:navBarTextColor          "#fff"
-;;                      :navBarTransparent        true
-;;                      :navBarButtonColor        "#fff"
-;;                      :statusBarTextColorScheme "light"
-;;                      :screenColor "black"}}))
-
-;; (defn get-well-known-screen []
-;;   (fn []
-;;     [screen {:navigation-bar {:title "Favorite"
-;;                               :tintColor "#fff"}}
-;;      [well-known-words-comp]]))
-
 (defn well-known-tab-icon [props]
   (r/as-element
    (if (true? (.-focused props))
@@ -68,5 +50,4 @@
                      :tintColor "#fff"}
     :tab-bar {:label "Favorite"
               :icon well-known-tab-icon}}
-   [ui/view
-    [ui/text {:style {:color "white"}} "Well known"]]))
+   well-known-words-comp))
