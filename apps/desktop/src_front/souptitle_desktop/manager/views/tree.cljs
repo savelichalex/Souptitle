4dc6ff0fee71329459e4018c3c9e1b239d869d30
Alexey Savelev <savelichalex93@gmail.com>
2017-03-17 00:27:12 +0300

    desktop: Add styles a little to tree view

--- apps/desktop/src_front/souptitle_desktop/manager/views/tree.cljs
+++ apps/desktop/src_front/souptitle_desktop/manager/views/tree.cljs
@@ -9,14 +9,14 @@
     (fn [{:keys [content] :as node} node-comp tree]
       [:div
        [:div
-        (when (not (nil? content))
-          [:div
-           {:on-click #(swap! toggled? not)}
-           [icon :angle-right
-            {:rotate (and @toggled? 90)
-             :style {:float "left"
-                     :padding "0 5px"}}]])
-        (node-comp node)]
+        (node-comp
+         node
+         (when (not (nil? content))
+           [:div
+            {:on-click #(swap! toggled? not)
+             :style {:padding-right "5px"}}
+            [icon :angle-right
+             {:rotate (and @toggled? 90)}]]))]
        (when (not (nil? content))
          [tree content node-comp @toggled?])])))
 
@@ -27,36 +27,46 @@
              (map #(identity [tree-node % node-comp tree-comp])))))
 
 (defn tree [{:keys [content label-comp]}]
-  [tree-comp content label-comp true])
+  [:div {:style {:background-color "#303945"
+                 :height "100%"}}
+   [tree-comp content label-comp true]])
 
 (defstyle style
   [".node" {:font-size "14px"
             :font-family "Roboto"
             :padding "8px"
             :cursor "pointer"
-            :transition "all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms"}
-   ["&:hover" {:background-color "#ccc"}]]
-  [".active" {:background-color "#eee"}]
+            :transition "all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms"
+            :display "flex"
+            :flex-direction "row"
+            :align-items "center"
+            :color "#aabac7"}
+   ["&:hover" {:background-color "#1c232d"}]]
+  [".active" {:background-color "#242d3a"
+              :color "#fff"}]
   [".season" {:padding-left "18px"}]
-  [".chapter" {:padding-left "28px"}])
+  [".chapter" {:padding-left "38px"}])
 
 (defmulti node (fn [el] (-> el :meta :type)))
 
-(defmethod node :serial [{{:keys [title]} :meta id :id active? :active?}]
+(defmethod node :serial [{{:keys [title]} :meta id :id active? :active?} shevron]
   [:div
    {:on-click #(dispatch [:set-active-serial id])
     :class (str (:node style) " " (when active? (:active style)))}
-   [icon :television]
+   shevron
+   ;; [icon :television {:style {:padding-right "5px"}}]
    title])
 
-(defmethod node :season [{{:keys [title]} :meta id :id active? :active?}]
+(defmethod node :season [{{:keys [title]} :meta id :id active? :active?} shevron]
   [:div
    {:on-click #(dispatch [:set-active-season id])
     :class (str (:node style) " " (:season style) " " (when active? (:active style)))}
+   shevron
    title])
 
-(defmethod node :chapter [{{:keys [title]} :meta id :id active? :active?}]
+(defmethod node :chapter [{{:keys [title]} :meta id :id active? :active?} shevron]
   [:div
    {:on-click #(dispatch [:set-active-chapter id])
     :class (str (:node style) " " (:chapter style) " " (when active? (:active style)))}
+   shevron
    title])
